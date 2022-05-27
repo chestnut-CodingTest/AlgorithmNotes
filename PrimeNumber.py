9d3ee92e789866bf5f44ac47e7b82f2b633f6e34
chestnut1044 <wbk1620@gmail.com>
2022-05-27 22:41:45 +0900

    소수 판별(에라토스테네스의 체)

---
+++ PrimeNumber.py
@@ -0,0 +1,9 @@
+arr = [True] * (n+1)
+for i in range(2, int(n ** 0.5)+1):
+    if arr[i] == True:
+        for j in range(i+i, n+1, i):
+            arr[j] = False
+
+for i in range(2, n+1):
+    if arr[i]:
+        print(i)
